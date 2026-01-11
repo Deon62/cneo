258d8b92d05c86a085c0f4acf54358015ebb370e
Deon62 <orinadeon@gmail.com>
2026-01-12 01:59:33 +0300

    updated hero in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import './NavbarMobile.css'
 
 function Navbar() {
   const location = useLocation()
